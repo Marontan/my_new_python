bf3ce0d5cd53ae0410984fb394e509daae448b93
student <masha_gold@mail.ru>
2019-09-12 16:18:08 +0700

    Сеть в графе, рекомендация связей

--- Vpythone.py
+++ Vpythone.py
@@ -1,152 +1,62 @@
 from datetime import date
 import csv
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class User:
     def __init__(self, _name, _birthday):
         self.name = _name
         self.date_of_dirth = date(_birthday[0], _birthday[1], _birthday[2])
-        self.friends = []
-
     def get_age(self):
         return int((date.today() - self.date_of_dirth).days / 365)
 
-    def add_friends(self, _user):
-        self.friends.append(_user)
-
-    def get_friends(self):
-        return self.friends
-    def __str__(self):
-        return f"This is {self.name}"
-    def __len__(self):
-        return len(self.friends)
-    def __ge__(self, other):
-        return len(self.friends) >= len(other.friends)
-    def __gt__(self, other):
-        return len(self.friends) > len(other.friends)
-    def __le__(self, other):
-        return len(self.friends) <= len(other.friends)
-    def __lt__(self, other):
-        return len(self.friends) < len(other.friends)
-    def __eq__(self, other):
-        return len(self.friends) == len(other.friends)
-
-    def print_friends(self):
-        print(f"Friends of {self.name}:")
-        for us in self.friends:
-            print(f"\t User {us.name}, birthday {us.date_of_dirth}")
-
-class Avatar():
-    def add_avatar(self, avatar):
-        self.avatar = avatar
-
-class PremiumMode():
-    def __init__(self):
-        self.premium_mode = False
-
-    def enable_premium_mode(self):
-        self.premium_mode = True
-
-class Author(User, Avatar, PremiumMode):
-    def __init__(self, _name, _birthday):
-        super().__init__(_name, _birthday)
-        self.posts = []
-
-    def add_post(self, text):
-      self.posts.append(text)
-
-    def remove_post(self, index):
-        if index > 0 and index < len(self.posts)+1:
-            self.posts.pop(index-1)
-
-
 class SocialNetwork:
-    users=[]
+    def __init__(self):
+        self.graph_users = nx.Graph()
 
     def init_users(self):
-        # 1
-        user_vic = User("Vic", [1999, 1, 23])
-        user_pete = User("Pete", [1989, 8, 4])
-        user_ross = User("Lis", [1995, 3, 12])
-        # 2
-        user_vic.add_friends(user_ross)
-        user_vic.add_friends(user_pete)
-        # user_vic.print_friends()
+        self.add_user(User("Rachel", [1999, 1, 23]))
+        self.add_user(User("Monika", [1989, 8, 4]))
+        self.add_user(User("Phoebe", [1995, 3, 12]))
+        self.add_user(User("Ross", [1982, 7, 3]))
+        self.add_user(User("Chandler", [1984, 9, 23]))
+        self.add_user(User("Joey", [1985, 11, 14]))
 
-        user_ross.add_friends(user_vic)
-        user_ross.add_friends(user_pete)
-        # user_ross.print_friends()
+        self.add_friendship("Rachel", "Monika")
+        self.add_friendship("Rachel", "Chandler")
+        self.add_friendship("Phoebe", "Joey")
+        self.add_friendship("Phoebe", "Rachel")
+        self.add_friendship("Chandler", "Ross")
+        self.add_friendship("Chandler", "Phoebe")
 
-        user_pete.add_friends(user_vic)
-        user_pete.add_friends(user_ross)
-        # user_pete.print_friends()
+    def get_neighbours(self, user):
+         return set(self.graph_users.neighbors(user))
 
-        self.users.append(user_vic)
-        self.users.append(user_pete)
-        self.users.append(user_ross)
-        # 3
-        # print(f"Name first user: {self.users[0].name}")
-        # 4
-        # print(f"Age of {self.users[1].name} is {self.users[1].get_age()} years")
-        # 5
-        # print(f"Count friends of {self.users[1].name} {len(self.users[2].friends)}")
+    def recommend_friends(self, user):
+        result_recommend = set()
+        friends = self.get_neighbours(user)
+        for friend in friends:
+            friend_friend = self.get_neighbours(friend)
+            for item in friend_friend:
+                result_recommend.add(item)
+        result_recommend = result_recommend.difference(friends)
+        result_recommend.remove(user)
+        return result_recommend
 
     def add_user(self, user):
-        self.users.append(user)
+        self.graph_users.add_node(user.name, data=user)
 
-    def __getitem__(self, item):
-        for i in self.users:
-            if i.name == item:
-                return i
-        return None
-
-    def export_users_to_json(self):
-        with open("users.csv", "wt") as myFile:
-            fieldnames = ["name", "birthday"]
-            writer = csv.DictWriter(myFile, fieldnames = fieldnames)
-            writer.writeheader()
-            for us in self.users:
-                writer.writerow({'name': us.name, 'birthday': f" {str(us.date_of_dirth)} "})
+    def add_friendship(self, user1, user2):
+        self.graph_users.add_edge(user1, user2)
 
 
-
-
-authorRoss = Author("Ross", [1982, 7, 3])
-authorCh = Author("Chendler", [1984, 9, 23])
-authorCh.add_avatar("avatar")
-authorCh.add_friends(authorRoss)
-authorJoe = Author("Joe", [1985, 11, 14])
-authorJoe.enable_premium_mode()
 
 network = SocialNetwork()
 network.init_users()
-network.add_user(authorRoss)
-network.add_user(authorCh)
-network.add_user(authorJoe)
 
-network.export_users_to_json()
+userFind = "Rachel"
+print(f"Friends of {userFind}:", network.recommend_friends(userFind))
 
+nx.draw_networkx(network.graph_users)
+plt.show()
 
-
-class Car:
-    def __init__(self, *args):
-        self.name = args[0]
-        self.weight = args[1]
-
-    def __str__(self):
-           return f"This is {self.name}"
-    def __ge__(self, other):
-        return self.weight >= other.weight
-    def __gt__(self, other):
-        return self.weight > other.weight
-    def __le__(self, other):
-        return self.weight <= other.weight
-    def __lt__(self, other):
-        return self.weight < other.weight
-    def __len__(self):
-        return len(self.name)
-
-carD = Car("D", 1)
-carF = Car("F", 4)
-# print(carD < carF)
-# print(len(carD))
-# list(filter(lambda x: x == "Alex", user))
